d89d147849ca3a92bd2f333cd906add25465bafc
sdmotto <119888788+sdmotto@users.noreply.github.com>
2024-11-10 15:50:26 -0600

    validated user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
 end
