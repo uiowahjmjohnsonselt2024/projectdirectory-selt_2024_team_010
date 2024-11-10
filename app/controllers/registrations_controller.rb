2581d7493f01de8818bcc9b05ad46922ff0a8233
sdmotto <119888788+sdmotto@users.noreply.github.com>
2024-11-10 16:10:21 -0600

    Added basic login and registration capabilities

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,24 @@
 class RegistrationsController < ApplicationController
 
   def new
+    @user = User.new
   end
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id  # Log the user in immediately after registration
+      flash[:notice] = "Registration successful"
+      redirect_to dashboard_path
+    else
+      flash.now[:alert] = "Error creating account"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:username])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to dashboard_path
+    else
+      flash[:alert] = "Invalid username or password"
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully"
+    redirect_to welcome_path
   end
 end
