6ee7a1dbed646f45a22cda6d139910d9c0905832
Jared Mulder <jared-mulder@uiowa.edu>
2024-11-13 10:05:34 -0600

    Added the create_session_token method along with the validations for the existence+uniqueness of the token and existence of the Foreign Key.

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,8 @@
 class Session < ActiveRecord::Base
   belongs_to :user
+  validates :session_token, presence: true, uniqueness: true #Never have multiple sessions with the same token!
+  validates :user_id, presence: true
+  def create_session_token
+    self.session_token = SecureRandom.urlsafe_base64
+  end
 end
