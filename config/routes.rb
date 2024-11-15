352c15fcdc1deb40bf0591271289ec7cadce81f3
sdmotto <119888788+sdmotto@users.noreply.github.com>
2024-11-14 20:24:26 -0600

    improved user validations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: redirect('/welcome')
 
   get 'login', to: 'sessions#new', as: 'login'
-  post 'login', to: 'sessions#create', as: 'login_create'
+  post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
 
   get 'register', to: 'registrations#new', as: 'register'
