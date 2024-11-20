93b4198badbed0aff24e7499415a7c355ad3314b
sdmotto <119888788+sdmotto@users.noreply.github.com>
2024-11-19 18:51:53 -0600

    added work around from HW6

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,7 @@ SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
