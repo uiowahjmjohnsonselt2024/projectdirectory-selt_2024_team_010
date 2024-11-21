fa61188b6a71d266aed1af5e5d9f462e92b477bd
sdmotto <119888788+sdmotto@users.noreply.github.com>
2024-11-21 13:41:37 -0600

    added ability to reset username and password

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,4 +3,38 @@ class SettingsController < ApplicationController
   def index
     @current_user = current_user
   end
+
+  def update_username
+    user = User.find(current_user.id)
+
+    if user.update(username: params[:new_username])
+      flash[:notice] = "Username successfully updated!"
+    else
+      flash[:alert] = "Failed to update username. #{user.errors.full_messages.join(', ')}"
+    end
+
+    redirect_to settings_path
+  end
+
+  def update_password
+    user = User.find(current_user.id)
+
+    unless user.authenticate(params[:current_password])
+      flash[:alert] = "Current password is incorrect."
+      redirect_to settings_path and return
+    end
+
+    if params[:new_password] != params[:confirm_password]
+      flash[:alert] = "New password and confirmation do not match."
+      redirect_to settings_path and return
+    end
+
+    if user.update(password: params[:new_password])
+      flash[:notice] = "Password successfully updated!"
+    else
+      flash[:alert] = "Failed to update password. #{user.errors.full_messages.join(', ')}"
+    end
+
+    redirect_to settings_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy', as: 'logout'
 
   get 'settings', to: 'settings#index', as: 'settings'
+  post 'users/reset_username', to: 'settings#update_username', as: 'update_username'
+  post 'users/reset_password', to: 'settings#update_password', as: 'update_password'
 
   get 'register', to: 'registrations#new', as: 'register'
   post 'register', to: 'registrations#create'
