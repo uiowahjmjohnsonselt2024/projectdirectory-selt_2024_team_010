86d839ce2e6a28746e460844a0ab0746abd56aa0
sdmotto <119888788+sdmotto@users.noreply.github.com>
2024-11-22 12:32:33 -0600

    fixed shop controller spec

--- spec/controllers/shop_controller_spec.rb
+++ spec/controllers/shop_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ShopController, type: :controller do
+  let(:user) { User.create!(username: 'testuser', email: 'test@example.com', password: 'password123', password_confirmation: 'password123') }
+  let(:session_token) { SecureRandom.urlsafe_base64 }
+
+  before do
+    user.create_session!(session_token: session_token)
+  end
+
   describe 'GET #index' do
     it 'returns a successful response' do
       get :index
