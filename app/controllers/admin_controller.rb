2797c1784e0dcf663cccc0276428c2fca44e8368
sdmotto <119888788+sdmotto@users.noreply.github.com>
2024-11-27 15:05:34 -0600

    added ability for admins to add users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,13 +12,13 @@ class AdminController < ApplicationController
   def edit_user_form
     @user = User.find(params[:id])
 
-    if @user.update(user_params)
+    if @user.update(params.require(:user).permit(:username, :email, :shard_amount, :money_usd, :isAdmin))
       flash[:notice] = "User updated successfully"
+      redirect_to admin_path
     else
-      flash[:alert] = "Failed to update user: #{@user.errors.full_messages.to_sentence}"
+      flash.now[:alert] = "Failed to update user: #{@user.errors.full_messages.to_sentence}"
+      render :edit_user
     end
-
-    redirect_to admin_path
   end
 
   def add_user
@@ -26,12 +26,14 @@ class AdminController < ApplicationController
   end
 
   def add_user_form
-    redirect_to admin_path
-  end
+    @user = User.new(params.require(:user).permit(:username, :email, :password, :shard_amount, :money_usd, :isAdmin))
 
-  private
-
-  def user_params
-    params.require(:user).permit(:username, :email, :shard_amount, :money_usd, :isAdmin)
+    if @user.save
+      flash[:notice] = "User created successfully"
+      redirect_to admin_path
+    else
+      flash.now[:alert] = "Failed to create user: #{@user.errors.full_messages.to_sentence}"
+      render :add_user
+    end
   end
 end
