571c1b11be81d62c893e550627c15e11aa95a24b
Jared Mulder <jared-mulder@uiowa.edu>
2024-11-29 13:36:22 -0600

    Added manual reference for most recent character. This is unsafe but the safer method only exists in rails 5.0+ so we have no choice.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20241129171529) do
+ActiveRecord::Schema.define(version: 20241129191645) do
 
   create_table "characters", force: :cascade do |t|
     t.integer "user_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20241129171529) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "session"
+    t.integer  "recent_character_id_id"
   end
 
 end
