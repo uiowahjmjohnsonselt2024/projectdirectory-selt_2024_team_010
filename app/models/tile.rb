eae248df5e91e15d9789d6482dc3977f5c152a26
Jared Mulder <jared-mulder@uiowa.edu>
2024-12-04 10:00:17 -0600

    Corrected the uniqueness validation to allow the same X and Y as long as the game_id is distinct.

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,7 +1,7 @@
 class Tile < ActiveRecord::Base
   belongs_to :game
 
-  # X+Y are unique as a pair, but as long as X is unique, Y can be the same and vice versa.
-  validates :x_position, presence: true, uniqueness: { scope: :y_position }
+  # X must be unique with regards to Y and game_id so that no 2 tiles can have the same X,Y,game at the same time.
+  validates :x_position, presence: true, uniqueness: { scope: [:y_position, :game_id] }
   validates :y_position, presence: true
 end
