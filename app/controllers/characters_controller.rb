c45b3a0d84341e04c157472eed3551af4917a394
alexlehman99 <alexander-lehman@uiowa.edu>
2024-12-13 11:51:22 -0600

    Characters visually change when leaving/joining game

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -6,7 +6,9 @@ class CharactersController < ApplicationController
   def get_characters
     @characters = @current_game.characters
                                .select("characters.id, characters.x_position, characters.y_position, users.username")
-                               .joins(:user).all
+                               .joins(:user)
+                               .where("characters.id = users.recent_character")
+
     puts @characters.inspect
     render json: { characters: @characters }
   end
